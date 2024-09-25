4a077e4efd1eb6de0c3f9d5a3b05be222e4088d0
yuzuchan05 <j.teee.05@gmail.com>
2024-09-25 15:55:39 -0700

    update

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,35 +5,9 @@ $(".hamburger__icon").on("click", function (e) {
 });
 
 
-// aタグのクリックで該当セクションに飛ぶとき、セクションの始まりがheaderで隠れないようにする処理
-
-$('a[href^="#"]').click(function () {
-  var href = $(this).attr('href');
-  if ($(href).length) {
-      var browserWidth = window.outerWidth;
-
-      // 実際のヘッダーの高さを取得する（ヘッダーが固定されている場合）
-      var headerHeight = $('header').outerHeight(); // 'header'は実際のヘッダーのセレクタに変更
-
-      // レスポンシブな条件をそのまま残す
-      if (browserWidth < 768) {
-          headerHeight = 7 * (10 / 375 * browserWidth); // モバイル用のヘッダー高さ
-      } else if (browserWidth < 1024) {
-          headerHeight = 10.1 * (browserWidth / 100) + (browserWidth / 100);  // タブレット用
-      }
-
-      // デバッグ用：計算したheaderHeightを確認
-      console.log("Header Height:", headerHeight);
-
-      // アニメーションによるスクロール。ヘッダー分だけ上部にずらす
-      $('body,html').animate({
-          scrollTop: $(href).offset().top - headerHeight // ヘッダー高さを引く
-      }, 400, 'swing');
-
-      // ハンバーガーメニューを閉じる
-      $(".hamburger__nav").slideUp();
-      return false; // デフォルト動作を無効化
-  }
+// ハンバーガーメニューを閉じる
+$('.hamburger__nav a[href^="#"]').click(function () {
+  $(".hamburger__nav").slideUp();
 });
 
 
