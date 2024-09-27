7d2f4056107673203dc6b2019e9e615278b447bb
yuzuchan05 <j.teee.05@gmail.com>
2024-09-26 20:41:38 -0700

    update

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,10 +4,19 @@ $(".hamburger__icon").on("click", function (e) {
   $(".hamburger__nav").slideToggle();
 });
 
-
 // ハンバーガーメニューを閉じる
 $('.hamburger__nav a[href^="#"]').click(function () {
   $(".hamburger__nav").slideUp();
+});
+
+// リンクをクリックしたときの処理
+$('.a[href^="#"]').click(function (e) {
+  e.preventDefault(); // デフォルトのアンカー動作を無効化（トップに戻らないように）
+});
+
+// ハンバーガーメニュー以外のa要素はトップに戻らない
+$('a[href^="#"]').not('.hamburger__nav a').click(function (e) {
+  // 他のa要素では通常の動作を維持
 });
 
 
@@ -15,6 +24,7 @@ $('.hamburger__nav a[href^="#"]').click(function () {
 -----------------------------------------*/
 $(document).ready(function(){
   $('#smarttab').smartTab({
-      enableUrlHash: false
+      enableUrlHash: false,
+      autoAdjustHeight: false
   });
 });
